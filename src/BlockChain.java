a231e6db4029a52af9c278c8124d2765220c8c05
juan pablo ospina henao <jampol_1@hotmail.com>
2018-08-04 23:33:11 -0500

    test

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class BlockChain {
 	
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
-	public static int difficulty = 1;
+	public static int difficulty = 1; //lol
 
 	public static void main(String[] args) {	
 		//add our blocks to the blockchain ArrayList:
